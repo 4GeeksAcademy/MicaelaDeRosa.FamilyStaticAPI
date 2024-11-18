019a0cd73c76d575a28585c8aa9f1b1ec3209d1b
MicaelaDeRosa23 <micaaderosa@gmail.com>
2024-11-18 04:59:38 +0000

    MicaelaDeRosa.FamilyStaticAPI

--- src/app.py
+++ src/app.py
@@ -27,16 +27,37 @@ def sitemap():
 
 @app.route('/members', methods=['GET'])
 def handle_hello():
-
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
-
-
+    response_body = members
     return jsonify(response_body), 200
+
+
+@app.route('/members', methods=['POST'])
+def post_member():
+   body = request.get_json(silent=True)
+   if body is None:
+      return jsonify({"message" : "Send information to body"})
+   if "first_name" not in body:
+      return jsonify({"message" : "the field first_name is required"})
+   if "age" not in body:
+      return jsonify({"message" : "the field age is required"})
+   if "lucky_numbers" not in body:
+      return jsonify({"mesage" : "the field lucky_numbers is required" })
+   new_member= {
+                      'id': jackson_family._generateId(),
+                'first_name': body ["first_name"],
+                'last_name': jackson_family.last_name,
+                'age': body["age"],
+                'lucky_numbers': body["lucky_numbers"],
+   }
+   members = jackson_family.add_member(new_member)
+   return jsonify({"message" : "Good" , "members": members})
+
+   
+   
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -1,11 +1,3 @@
-
-"""
-update this file to implement the following already declared methods:
-- add_member: Should add a member to the self._members list
-- delete_member: Should delete a member from the self._members list
-- update_member: Should update a member from the self._members list
-- get_member: Should return a member from the self._members list
-"""
 from random import randint
 
 class FamilyStructure:
@@ -13,15 +5,38 @@ class FamilyStructure:
         self.last_name = last_name
 
         # example list of members
-        self._members = []
-
+        self._members = [
+            {
+                'id': self._generateId(),
+                'first_name': 'John',
+                'last_name': self.last_name,
+                'age': '33',
+                'lucky_numbers': [7, 13, 22]
+            },
+            {
+                'id': self._generateId(),
+                'first_name': 'Jane',
+                'last_name': self.last_name,
+                'age': '35',
+                'lucky_numbers': [10, 4, 3]
+            },
+            {
+                'id': self._generateId(),
+                'first_name': 'Jimmy',
+                'last_name': self.last_name,
+                'age': '5',
+                'lucky_numbers': [1]
+            }
+        ]
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
         return randint(0, 99999999)
 
     def add_member(self, member):
+        self._members.append(member)
+        return self._members
         # fill this method and update the return
-        pass
+
 
     def delete_member(self, id):
         # fill this method and update the return
