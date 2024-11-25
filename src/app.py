4699fdf7ca13ce1ddc3f2fcd96afd8ed6e9d4c8f
MicaelaDeRosa23 <micaaderosa@gmail.com>
2024-11-25 16:44:03 +0000

    MicaelaDeRosa.FamilyStaticAPI

--- src/app.py
+++ src/app.py
@@ -2,6 +2,20 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
+from flask import Flask, request, jsonify, url_for, Response
+from flask_cors import CORS
+from utils import APIException, generate_sitemap
+from datastructures import FamilyStructure
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+CORS(app)
+
+"""
+This module takes care of starting the API Server, Loading the DB and Adding the endpoints
+"""
+import os
 from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
@@ -26,60 +40,29 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_members():
-    # this is how you can use the Family datastructure by calling its methods
+def handle_hello():
+
+# this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    return jsonify(members), 200
-
-@app.route('/member/<int:id>', methods=["GET"])
-def get_single_member(id):
-    member = jackson_family.get_member(id)
-
-    if (member != None and member != {}):
-
-        return jsonify(member), 200
-    else:
-        return jsonify({"message": "member no found"}),200
-
-
-
-@app.route('/member', methods=['POST'])
-def hanlde_member():
-   body = request.get_json(silent=True)
-   if body is None:
-      return jsonify({"message" : "Send information to body"})
-   if "first_name" not in body:
-      return jsonify({"message" : "the field first_name is required"})
-   if "age" not in body:
-      return jsonify({"message" : "the field age is required"})
-   if "lucky_numbers" not in body:
-      return jsonify({"mesage" : "the field lucky_numbers is required" })
-   new_member= {
-                      'id': jackson_family._generateId(),
-                'first_name': body ["first_name"],
-                'last_name': jackson_family.last_name,
-                'age': body["age"],
-                'lucky_numbers': body["lucky_numbers"],
-   }
-   members = jackson_family.add_member(new_member)
-   return jsonify({"message" : "Good" , "members": members})
-
-
-
-@app.route('/member/<int:id>', methods=['DELETE'])
-def delete_member(id):
-    member = jackson_family.get_member(id)
-
-    if member:
-       jackson_family.delete_member(id)
-       return jsonify({"message": f"member delete successfully :{member}"}),200
-    else:
-       return jsonify({"error" :"member not found"}),200
-   
-
-
+    return jsonify(members),200
     
 
+@app.route('/member', methods=['POST'])
+def add_member():
+# this is how you can use the Family datastructure by calling its methods
+    new_member_data = request.json
+    jackson_family.add_member(**new_member_data)
+    return "Correcto"
+  
+
+@app.route('/member/<int:member_id>', methods=['GET','DELETE'])
+def get_member(member_id):
+    if request.method == 'GET':
+        member = jackson_family.get_member(member_id)
+        return jsonify(member)
+
+    jackson_family.delete_member(member_id)
+    return jsonify({"done": True})
 
 
 # this only runs if `$ python src/app.py` is executed
--- src/datastructures.py
+++ src/datastructures.py
@@ -1,50 +1,60 @@
 from random import randint
 
 class FamilyStructure:
+
     def __init__(self, last_name):
         self.last_name = last_name
 
         # example list of members
         self._members = [
             {
-                'id': self._generateId(),
-                'first_name': 'John',
-                'last_name': self.last_name,
-                'age': '33',
-                'lucky_numbers': [7, 13, 22]
+                "id": 1,
+                "fist_name": "John",
+                "last_name": self.last_name,
+                "age": 33,
+                "lucky_numbers": [7, 13, 22]
             },
-            {
-                'id': self._generateId(),
-                'first_name': 'Jane',
-                'last_name': self.last_name,
-                'age': '35',
-                'lucky_numbers': [10, 4, 3]
+            { 
+                "id": 2,
+                "first_name": "Jane",
+                "last_name": self.last_name,
+                "age": 35,
+                "lucky_numbers": [10, 14, 3]
             },
-            {
-                'id': self._generateId(),
-                'first_name': 'Jimmy',
-                'last_name': self.last_name,
-                'age': '5',
-                'lucky_numbers': [1]
-            }
+            { 
+                "id": 3,
+                "fist_name": "Jimmy",
+                "last_name": self.last_name,
+                "age": 5,
+                "lucky_numbers": 1
+            },
         ]
-    # read-only: Use this method to generate random members ID's when adding members into the list
+
+  # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
         return randint(0, 99999999)
 
-    def add_member(self, member):
-        self._members.append(member)
-        return self._members
+    def add_member(self, **member):
         # fill this method and update the return
-
+       self._members.append({
+            "first_name" : member['first_name'],
+            "id": member['id'],
+            "age": member['age'],
+            "lucky_numbers" : member['lucky_numbers']
+        })
 
     def delete_member(self, id):
         # fill this method and update the return
-        pass
+        for member in self._members:
+            if member['id'] == id:
+                self._members.remove(member)
+                
 
     def get_member(self, id):
         # fill this method and update the return
-        pass
+        for member in self._members:
+            if member['id'] == id:
+                return member
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
